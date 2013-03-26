11b875d5a4606faf0e1d6e2097f2e6b4b56cca4a
Yves Ledermann <yves@laedis.ch>
2013-03-26 15:19:21 +0100

    autoformating

--- main.c
+++ main.c
@@ -2,57 +2,57 @@
 
 int main()
 {
-/**< Haupt-Menü Anzeigen */
-
-int iMainMenuSelection=0;
-int iMainMenuExit=0;
-int iMainMenuFileLoaded=0;/**< Datei erfolgreich geladen */
-int iRow=0;
-int iValue=0;
 
 
-do // MainMenu
-{
-   iMainMenuSelection=menu_main(iMainMenuFileLoaded);
+    int iMainMenuSelection=0;
+    int iMainMenuExit=0;
+    int iMainMenuFileLoaded=0;/**< Datei erfolgreich geladen */
+    int iRow=0;
+    int iValue=0;
 
-    switch (iMainMenuSelection)
+    /**< Haupt-Menü Anzeigen */
+    do // MainMenu
     {
-    case 1: //Einlesen der Datei mit den Messwerte
-        printf("1 Einlesen der Datei mit den Messwerten\n");
+        iMainMenuSelection=menu_main(iMainMenuFileLoaded);
 
-        //Abfrage des Dateinamens
+        switch (iMainMenuSelection)
+        {
+        case 1: //Einlesen der Datei mit den Messwerte
+            printf("1 Einlesen der Datei mit den Messwerten\n");
 
-        //Übergabe des Dateinamens an readFile();
+            //Abfrage des Dateinamens
 
-        //Rückgabewert auswerten
+            //Übergabe des Dateinamens an readFile();
 
-        iMainMenuFileLoaded=1;
-        break;
+            //Rückgabewert auswerten
 
-    case 2: //Ausgeben der Messwerte am Bildschirm
-         printf("2 Ausgeben der Messwerte am Bildschirm\n");
-         break;
+            iMainMenuFileLoaded=1;
+            break;
 
-    case 3: //Auswertung der Messwerte
-         printf("3 Auswertung der Messwerte\n");
-         iRow=SelectRow();
-         iValue=SelectValue();
-         Analyse(iRow, iValue);
+        case 2: //Ausgeben der Messwerte am Bildschirm
+            printf("2 Ausgeben der Messwerte am Bildschirm\n");
+            break;
 
-         break;
+        case 3: //Auswertung der Messwerte
+            printf("3 Auswertung der Messwerte\n");
+            iRow=SelectRow();
+            iValue=SelectValue();
+            Analyse(iRow, iValue);
 
-    case 4: //Exportieren der Messwerte
-        printf("4 Exportieren der Messwerte\n");
-        break;
-    case 5: //Beenden des Programms
-        iMainMenuExit=1;
-        break;
+            break;
+
+        case 4: //Exportieren der Messwerte
+            printf("4 Exportieren der Messwerte\n");
+            break;
+        case 5: //Beenden des Programms
+            iMainMenuExit=1;
+            break;
+        }
+
+
+
     }
-
-
-
-}
-while (!iMainMenuExit);
+    while (!iMainMenuExit);
 
     return 0;
 }

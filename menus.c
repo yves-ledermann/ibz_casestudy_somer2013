4d17d174f3aa5665e35578b73db3b00e04e76d17
Yves Ledermann <yves@laedis.ch>
2013-04-10 23:35:07 +0200

    ascii banner

--- menus.c
+++ menus.c
@@ -25,6 +25,18 @@ int HauptMenu(int iDateiGeladen)
 //Bildschirm leeren
     CLEAR
 
+
+ printf(" _    _                   _                        _   _ \n");
+ printf("| |  | |                 | |                      (_) (_)\n");
+ printf("| |__| | __ _ _   _ _ __ | |_ _ __ ___   ___ _ __  _   _ \n");
+ printf("|  __  |/ _` | | | | '_ \\| __| '_ ` _ \\ / _ \\ '_ \\| | | |\n");
+ printf("| |  | | (_| | |_| | |_) | |_| | | | | |  __/ | | | |_| |\n");
+ printf("|_|  |_|\\__,_|\\__,_| .__/ \\__|_| |_| |_|\\___|_| |_|\\__,_|\n");
+ printf("                    | |                                  \n");
+ printf("                    |_|                                   \n");
+ printf("\n");
+
+
     printf("1 Einlesen der Datei mit den Messwerten\n");
     if (iDateiGeladen==6)
     {

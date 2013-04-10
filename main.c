f30d47184ecb472981061610e82f3a3d7436d691
Yves Ledermann <yves@laedis.ch>
2013-04-10 23:36:09 +0200

    update

--- main.c
+++ main.c
@@ -36,6 +36,7 @@ int main(int argc, char *argv[])
     //Haupt-Menü Anzeigen
     do // MainMenu
     {
+
         iHauptMenuAuswahl=HauptMenu(iEinlesenStatus);
 
         switch (iHauptMenuAuswahl)
